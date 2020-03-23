9219138b8e38f9a48d4b2deea4d6885f55d9dfc3
nachiketkallapur <nachiketkallapur@gmail.com>
2020-03-23 22:27:45 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -4,27 +4,19 @@ import './App.css';
 import Counter from './components/count-component';
 import EventHandling from './components/event-handling';
 import Parent from './components/parent-component';
+import Events from './components/events-component';
+import LifecycleA from './components/lifecycle-component/lifecycleA';
 
 
 function App() {
-  const [text,setText]=useState('');
-  const [showChild,setChild]=useState(true);
-  const clickHandlerForToggleEvents=()=>{
-    setChild(!showChild);
-  }
-  const clickHandlerForShowChild=()=>{
-    setText(text+"Nachiket");
-  }
+ 
   
   return (
     <div className="App">
       {/* <Counter/>
       <EventHandling/>0 */}
-      <button onClick={clickHandlerForToggleEvents}>Toggle events</button><br/>
-      <button onClick={clickHandlerForShowChild}>ShowChild</button>
-      <h3>{showChild?text:''}</h3>
-      <Parent/>
-             
+      <Events/>
+      <LifecycleA/>
     </div>
   );
 }
